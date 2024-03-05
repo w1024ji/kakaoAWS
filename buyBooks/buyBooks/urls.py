a91531b304adf805b45cc7379d33d2b0ff250ddf
Wonji Lee <w1024ji@gmail.com>
2024-03-05 18:53:49 +0900

    CONFIRM mainPage

--- buyBooks/buyBooks/urls.py
+++ buyBooks/buyBooks/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# buyBooks/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('mainPage.urls')), # 기본 경로는 main page가 맡는다
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- buyBooks/mainPage/views.py
+++ buyBooks/mainPage/views.py
@@ -1,3 +1,14 @@
+# mainPage/views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def mainPage(request):
+    return render(request, 'mainPage/mainPage.html')
+
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
 # Create your views here.
